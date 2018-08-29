952604e4a1d55503312f4a09a91df4b24a6d4727
Chang-soo Han <are@spoqa.com>
2018-08-29 21:21:29 +0900

    Add full chain api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import uuid
 
-from flask import Flask
+from flask import Flask, Response, jsonify
+from typeguard import typechecked
 
 from uaena.block_chain import BlockChain
 
@@ -9,6 +10,16 @@ app = Flask(__name__)
 node_identifier = str(uuid.uuid4()).replace('-', '')
 
 block_chain = BlockChain()
+
+
+@app.route('/chain/')
+@typechecked
+def full_chain() -> Response:
+    response = {
+        'chain': [block.serialize() for block in block_chain.chain],
+        'length': len(block_chain.chain),
+    }
+    return jsonify(response)
 
 
 if __name__ == '__main__':
